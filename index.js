f2f5e75076a0591b71162059ba3dcf8b6beb4471
ivan <ivan-guita@hotmail.com>
2024-01-11 21:59:17 -0300

    bootstrap instalado

--- index.js
+++ index.js
@@ -3,35 +3,16 @@ const app = express();
 
 // avisando que eu vou usar o EJS como view engine 
 app.set('view engine','ejs');
+//avisando onde fica a pasta do meu css e js estico
+app.use(express.static('public'));
 
 
-app.get('/:nome/:lang',(req,res)=>{
+app.get('/',(req,res)=>{
 
-    //o render vai direto pra pasta views por isso não é preciso colocar a url completa 
-    //mas caso o aruivo esteja dentro de uma subpasta da view ai vei ser preciso 
-    //colocar a url apartir da view
-
-    //passando variaveis junto com a view 
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-
-    var produtos = [
-        {nome:'Coca-lata', preco:'5,00'},
-        {nome:'Pastel de Carne', preco:'4,50'},
-        {nome:'Pizza de Carne', preco:'24,50'}
-
-    ]
+   
 
 
-    res.render('principal/Perfil',{
-        nome: nome,
-        lang: lang,
-        empresa:'Guia do programador',
-        inscritos: 8040,
-        msg: false,
-        produtos: produtos
-
-    });
+    res.render('index',);
 
 
 });
